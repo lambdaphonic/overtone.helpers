c75686a876f314e340f5479c859ee35be4a2ed67
Martin Gondermann <magicmonty@pagansoft.de>
2015-05-31 14:37:19 +0200

    Adds function spb, mspb, atbeat

--- src/lambdaphonic/overtone/helpers.clj
+++ src/lambdaphonic/overtone/helpers.clj
@@ -21,3 +21,37 @@
 
 (defn qcosr [scale beat r center freq]
   (quantize scale (cosr beat r center freq)))
+
+(defn spb
+  "
+  Returns the second per beat for the given metronome
+
+  for example
+  (def m (metronome 120)
+  (spb m)   --> 0.5
+  (spb m 1) --> 0.5
+  (spb m 2) --> 1.0
+  "
+  ([metro] (spb metro 1))
+  ([metro beat-offset]
+   (let [bpm (metro :bpm)
+         bps (/ bpm 60)]
+     (* 1.0 (/ beat-offset bps)))))
+
+(defn mspb
+  "
+  Returns the milliseconds per beat for the given metronome
+
+  for example
+  (def m (metronome 120))
+  (mspb m)   --> 500.0
+  (mspb m 1) --> 500.0
+  (mspb m 2) --> 1000.0
+  "
+  ([metro] (mspb metro 1))
+  ([metro beat-offset]
+   (* 1000.0 (spb metro beat-offset))))
+
+(defn atbeat [metro beat]
+  "returns the time with the given beat offset for the given metronome"
+  (metro (+ (metro) beat)))
